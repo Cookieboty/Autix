f2b98d4f1799b9ec500882bbc4aeb7893c3377b2
cookieboty <cookieboty@qq.com>
2026-04-05 16:52:00 +0800

    feat: add packages/database with Prisma schema for RBAC

---
+++ packages/database/src/index.ts
@@ -0,0 +1,6 @@
+export { PrismaClient } from '@prisma/client';
+export type {
+  User, Department, Role, Permission, Menu,
+  UserRole, RolePermission, RoleMenu, UserSession,
+  UserStatus, PermissionAction, MenuType,
+} from '@prisma/client';
