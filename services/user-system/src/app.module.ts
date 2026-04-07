a97bfa5795da56f7645a7525e00123a9899c944e
cookieboty <cookieboty@qq.com>
2026-04-07 21:12:49 +0800

    chore: remove DepartmentModule

--- services/user-system/src/app.module.ts
+++ services/user-system/src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
-import { DepartmentModule } from './department/department.module';
 import { PermissionModule } from './permission/permission.module';
 import { RoleModule } from './role/role.module';
 import { MenuModule } from './menu/menu.module';
@@ -16,7 +15,6 @@ import { RegistrationModule } from './registration/registration.module';
     PrismaModule,
     AuthModule,
     UserModule,
-    DepartmentModule,
     PermissionModule,
     RoleModule,
     MenuModule,
