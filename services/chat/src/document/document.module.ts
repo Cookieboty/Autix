b33ad21880cb1ffd3c3ae51c5c4ff169da89b06d
cookieboty <cookieboty@qq.com>
2026-04-12 08:36:12 +0800

    feat(chat): register SearchService and SearchController in DocumentModule

--- services/chat/src/document/document.module.ts
+++ services/chat/src/document/document.module.ts
@@ -3,11 +3,13 @@ import { DocumentService } from './document.service';
 import { DocumentController } from './document.controller';
 import { ChunkService } from './chunk.service';
 import { EmbeddingService } from './embedding.service';
+import { SearchService } from './search.service';
+import { SearchController } from './search.controller';
 import { SseModule } from '../sse/sse.module';
 
 @Module({
   imports: [SseModule],
-  providers: [DocumentService, ChunkService, EmbeddingService],
-  controllers: [DocumentController],
+  providers: [DocumentService, ChunkService, EmbeddingService, SearchService],
+  controllers: [DocumentController, SearchController],
 })
 export class DocumentModule {}
